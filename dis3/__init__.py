4b1f35301f3f94060a80520762f8de592abc004f
KY _ <73789857+kapten-kaizo@users.noreply.github.com>
2021-12-19 00:32:09 +0700

    Add files via upload

--- dis3/__init__.py
+++ dis3/__init__.py
@@ -42,77 +42,83 @@ class Asm:
         self.ki=ki
         self.mm=lambda x:bytes(bytearray(x))
 
-    def kawai(self,c,b,a):
-        c1 = c.co_code
-        c2 = c.co_consts
-        if a in [1]:
-            c1=b
-        if a in [2]:
-            c2=b
-        data=[c.co_argcount,c.co_nlocals,c.co_stacksize,c.co_flags,c1,c2,c.co_names,c.co_varnames,c.co_filename,c.co_name,c.co_firstlineno,c.co_lnotab,c.co_freevars,c.co_cellvars]
+    def kawai(self, my_code, key_name, master_key):
+        arg1 = my_code.co_code
+        arg2 = my_code.co_consts
+        if master_key in [1]:
+            arg1 = key_name
+        if master_key in [2]:
+            arg2 = key_name
+        data=[my_code.co_argcount,my_code.co_nlocals,my_code.co_stacksize,my_code.co_flags,arg1,arg2,my_code.co_names,my_code.co_varnames,my_code.co_filename,my_code.co_name,my_code.co_firstlineno,my_code.co_lnotab,my_code.co_freevars,my_code.co_cellvars]
         try:
-            data[1:1] = [c.co_posonlyargcount,c.co_kwonlyargcount]
+            data[1:1] = [my_code.co_posonlyargcount,my_code.co_kwonlyargcount]
         except:
-            data.insert(1, c.co_kwonlyargcount)
+            data.insert(1, my_code.co_kwonlyargcount)
         return types.CodeType(*data)
 
     def get_byte_code(self, source):
-        s = re.findall("\((.*)\)", source)[0]
-        xs = "".join([chr(int(i)) for i in s.split(",")])
-        return marshal.loads(zlib.decompress(base64.b64decode(xs)))
+        yah_kok_knok = re.findall("\((.*)\)", source)[0].split(",")
+        sorry2_lag_cok = "".join([chr(int(yeah)) for yeah in yah_kok_knok])
+        return marshal.loads(zlib.decompress(base64.b64decode(sorry2_lag_cok)))
 
-    def regex(self, rb, line=''):
-        obj = []
-        split = rb.splitlines()
-        i = 0
-        while i < len(split):
-            x = split
-            if x[i].startswith(line):
-                i = i+1
-                continue
-            s = x[i].split()
-            wtf = len(s)
-            for y in range(wtf):
-                if s[y] in opmap.keys():
-                    obj.append(opmap[s[y]])
-                    obj.append(int(s[y+1]))
-                    break
-            i = i+1
-        return obj
-
-    def main_asm(self):
-        read = open(self.file).read().split(kapten)
-        module = self.get_byte_code(read[0])
-        load_const = list(module.co_consts)
-        ln = len(read)-1
+    def regex(self, rip_grep, offset=''):
+        this_fucker = None
+        if offset in [1]:
+            this_fucker = "sparator"
+        if offset in [2]:
+            this_fucker = 'have_code'
+        result_point = []
+        rg_split = rip_grep.splitlines()
         arg_repr = 0
-        while arg_repr < ln:
-            sc = read[arg_repr]
-            su = sc.splitlines()
-            if "const" and "<code object" in str(su):
-                k = re.findall("\((\d+)\)", str(su[1]))
-                ajg = len(k)
-                if ajg in [1]:
-                    i = int(k[0])
-                    xx=load_const[i]
-                    z = self.regex(sc, "const")
-                    kode=self.mm(z)
-                    load_const[i] = self.kawai(xx,kode,1)
-                if ajg in [2]:
-                    a = int(k[0])
-                    b = int(k[1])
-                    xz = load_const[a]
-                    zx = list(xz.co_consts)
-                    cs = zx[b]
-                    sb = self.regex(sc, "const")
-                    kode = self.mm(sb)
-                    zx[b] = self.kawai(cs,kode,1)
-                    load_const[a] = self.kawai(xz,tuple(zx),2)
-            arg_repr = arg_repr+1
-        ab = self.regex(read[1], "module")
-        kode = self.mm(ab)
-        xw = self.kawai(module, kode, 1)
-        return self.kawai(xw, tuple(load_const), 2)
+        offset_width = len(rg_split)
+        while arg_repr < offset_width:
+            key_name = rg_split
+            if key_name[arg_repr][:len(this_fucker)] in [this_fucker]:
+               arg_repr = arg_repr + 1
+               continue
+            awokawok = key_name[arg_repr].split()
+            sparator = len(awokawok)
+            for wibu in range(sparator):
+                if awokawok[wibu] in opmap.keys():
+                    result_point.append(opmap[awokawok[wibu]])
+                    result_point.append(int(awokawok[wibu+1]))
+                    break
+            arg_repr = arg_repr + 1
+        return result_point
+
+    def main_asm(self, arg=0, arg1=1, arg2=2):
+        read = open(self.file).read().split(kapten)
+        my_py_ini = self.get_byte_code(read[arg])
+        master_key = list(my_py_ini.co_consts)
+        offset_width = len(read) - arg1
+        arg_repr = arg
+        while arg_repr < offset_width:
+            arg_value = read[arg_repr]
+            arg_split = arg_value.splitlines()
+            if "sparator" and "<code object" in str(arg_split):
+                get_num_tuple = re.findall("\((\d+)\)", arg_split[arg1])
+                this_is_point = len(get_num_tuple)
+                if this_is_point in [arg1]:
+                    my_axist = int(get_num_tuple[arg])
+                    kode_keras = master_key[my_axist]
+                    int_bytecode = self.regex(arg_value, arg1)
+                    xl_prioritas = self.mm(int_bytecode)
+                    master_key[my_axist] = self.kawai(kode_keras, xl_prioritas, arg1)
+                if this_is_point in [arg2] or this_is_point > arg2:
+                    arg_func = int(get_num_tuple[arg])
+                    bokep_indo = int(get_num_tuple[arg1])
+                    sparator = master_key[arg_func]
+                    story_wa = list(sparator.co_consts)
+                    broken_heart = story_wa[bokep_indo]
+                    get_code_info = self.regex(arg_value, arg1)
+                    in_bytecode = self.mm(get_code_info)
+                    story_wa[bokep_indo] = self.kawai(broken_heart, in_bytecode, arg1)
+                    master_key[arg_func] = self.kawai(sparator, tuple(story_wa), arg2)
+            arg_repr = arg_repr + arg1
+        my_self = self.regex(read[arg1], arg2)
+        this_bytecode = self.mm(my_self)
+        my_telkomsel = self.kawai(my_py_ini, this_bytecode, arg1)
+        return self.kawai(my_telkomsel, tuple(master_key), arg2)
 
     def main(self):
         byte_code = self.main_asm()
@@ -126,62 +132,62 @@ class Dis:
     def __init__(self,code):
         self.kodek=code
 
-    def dis_py3(self, code):
-        aray = code.co_code
+    def dis_py3(self, this_code):
+        aray = this_code.co_code
         argval = list(aray)
-        i = 0
-        x = code
-        ln = len(argval)-1
-        while i < ln:
-            oparg = argval[i]
+        arg_repr = 0
+        length = len(argval) - 1
+        while arg_repr < length:
+            oparg = argval[arg_repr]
             op = opname[oparg]
-            arg = argval[i+1]
-            ajg=" ".join(["", repr(i), op, repr(arg)])
+            arg2_int = argval[arg_repr + 1]
+            this_key = " ".join(["", repr(arg_repr), op, repr(arg2_int)])
             if oparg in hasname:
-                print("%s (%s)"%(ajg, x.co_names[arg]))
+                print("%s (%s)"%(this_key, this_code.co_names[arg2_int]))
             elif oparg in haslocal:
-                print("%s (%s)"%(ajg, x.co_varnames[arg]))
+                print("%s (%s)"%(this_key, this_code.co_varnames[arg2_int]))
             elif oparg in hasconst:
-                print("%s (%s)"%(ajg, repr(x.co_consts[arg])))
+                print("%s (%s)"%(this_key, repr(this_code.co_consts[arg2_int])))
             elif oparg in hascompare:
-                print("%s (%s)"%(ajg, cmp_op[arg]))
-            else:print(ajg)
-            i = i+2
+                print("%s (%s)"%(this_key, cmp_op[arg2_int]))
+            else:print(this_key)
+            arg_repr = arg_repr + 2
         print(kapten)
 
-    def puki(self,y,z):
-        print(y)
-        self.dis_py3(z)
-
-    def try1(self,x,y=None):
-        ln = len(x)
-        for i in range(ln):
-            arg = x[i]
-            if type(arg) is types.CodeType:
-                self.puki(y%(str(i), str(arg)),arg)
-                self.try1(arg.co_consts, "const ("+str(i)+")(%s) %s")
+    def puki(self, this_text, this_code):
+        print(this_text)
+        self.dis_py3(this_code)
+        return list(this_code.co_consts)
 
     def main(self):
         print("# This Script Written By KangEhem")
         print("# Dont Forget To Follow My Github Profile !")
-        ag=base64.b64encode(zlib.compress(marshal.dumps(self.kodek)))
-        byte=list(ag)
-        list_byte = '(%s)'%','.join([str(i) for i in byte])
-        self.puki('%s\n%s\nmodule %s '%(list_byte,kapten,str(self.kodek)), self.kodek)
-        self.try1(self.kodek.co_consts, "const (%s) %s")
+        ag=list(base64.b64encode(zlib.compress(marshal.dumps(self.kodek))))
+        list_byte = '(%s)'%','.join([str(i) for i in ag])
+        aray=self.puki('%s\n%s\nhave_code %s '%(list_byte,kapten, str(self.kodek)), self.kodek)
+        length = len(aray)
+        for aargh in range(length):
+            argval = aray[aargh]
+            if type(argval) is types.CodeType:
+                array=self.puki("sparator (%s) %s"%(str(aargh), str(argval)), argval)
+                for aargh2 in range(len(array)):
+                    argval2 = array[aargh2]
+                    if type(argval2) is types.CodeType:
+                        self.puki("sparator (%s)(%s) %s" % (str(aargh), str(aargh2), str(argval2)), argval2)
 
-def load_module(fk):
+
+def load_module(Sparator):
     y = None
     try:
-        x = open(fk).read()
-        y = compile(x,fk,"exec")
+        x = open(Sparator).read()
+        y = compile(x, Sparator, "exec")
     except Exception as i:
         try:
-            f = open(fk, "rb")
-            f.seek(16)
-            y = marshal.loads(f.read())
+            x = open(Sparator, "rb")
+            x.seek(16)
+            y = marshal.loads(x.read())
         except Exception as i:
-            exit("Sepertinya ada kesalahan di file %s"%fk)
+            exit("Sepertinya ada kesalahan di file %s"%Sparator)
     return y
 
 def cekfile(f):
@@ -205,4 +211,5 @@ def menu(sys):
 def main():
     try:menu(sys.argv)
     except (KeyboardInterrupt, EOFError):exit()
+main()
 # Mau Nyari Apaan Sih Cuk?
